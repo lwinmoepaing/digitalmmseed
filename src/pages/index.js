c8dc2ca7904a0f3bded8be9adf68486b1a0a41e0
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2020-02-24 02:36:44 +0700

    Add Second row

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,73 +11,73 @@ import DataWorkingTree from '../components/Common/SVG/DataWorkingTree'
 import DataConfirm from '../components/Common/SVG/DataConfirm'
 import DataChoose from '../components/Common/SVG/DataChoose'
 
-const Index = ({ t }) => {
-  const name = 'Something'
-  return (
-    <Layout>
-      <div className="Container">
-        <HomeSectionOne />
+const Index = ({ t }) => (
+  <Layout>
+    <div className="Container">
+      <HomeSectionOne />
 
-        <div className="SectionTwoContainer">
-          <div className="HowItWorksContainer">
-            <h1>
-              How It Works
-            </h1>
-            <p>
-              Using practical experience in farming, our strategies are focused on
-              optimizing financial performance while achieving our goal of increasing local production.
-            </p>
-          </div>
-          <div style={{ width: '100%', overflow: 'hidden', padding: '0 1.5rem' }}>
-            <Row gutter={[16, 16]}>
-              <Col xs={{ span: 12 }} md={{ span: 6 }}>
-                <div className="CustomCardStyle">
-                  <p className="CardLeft">
-                    <DataChoose />
-                  </p>
-                  <p className="CardRight">
-                    1st Craeting Project By Farmers Or Our Customers
-                  </p>
-                </div>
-              </Col>
-              <Col xs={{ span: 12 }} md={{ span: 6 }}>
-                <div className="CustomCardStyle">
-                  <p className="CardLeft">
-                    <DataConfirm />
-                  </p>
-                  <p className="CardRight">
-                    2nd Contact Your Project And Our Staffs will check everything you okay
-                  </p>
-                </div>
-              </Col>
-              <Col xs={{ span: 12 }} md={{ span: 6 }}>
-                <div className="CustomCardStyle">
-                  <p className="CardLeft">
-                    <DataWorkingTree />
-                  </p>
-                  <p className="CardRight">
-                    3rd Woring Duration ( Farming or Animal Husbandry ) of Project
-                    & Get Money
-                  </p>
-                </div>
-              </Col>
-              <Col xs={{ span: 12 }} md={{ span: 6 }}>
-                <div className="CustomCardStyle">
-                  <p className="CardLeft">
-                    <DataSecurityGetMoney />
-                  </p>
-                  <p className="CardRight">
-                    4th After Work Done, You'll Get Secure 100% Get Money
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </div>
+      <div className="SectionTwoContainer">
+        <div className="HowItWorksContainer">
+          <h1>
+            How It Works
+          </h1>
+          <p>
+            Using practical experience in farming, our strategies are focused on
+            optimizing financial performance while
+            achieving our goal of increasing local production.
+          </p>
+        </div>
+        <div style={{ width: '100%', overflow: 'hidden', padding: '0 1.5rem' }}>
+          <Row gutter={[16, 16]}>
+            <Col xs={{ span: 12 }} md={{ span: 6 }}>
+              <div className="CustomCardStyle">
+                <p className="CardLeft">
+                  <DataChoose />
+                </p>
+                <p className="CardRight">
+                  1st, Craeting Project By Farmers Or Our Customers
+                </p>
+              </div>
+            </Col>
+            <Col xs={{ span: 12 }} md={{ span: 6 }}>
+              <div className="CustomCardStyle">
+                <p className="CardLeft">
+                  <DataConfirm />
+                </p>
+                <p className="CardRight">
+                  2nd, Contact Your Project And Our Staffs will check everything you okay
+                </p>
+              </div>
+            </Col>
+            <Col xs={{ span: 12 }} md={{ span: 6 }}>
+              <div className="CustomCardStyle">
+                <p className="CardLeft">
+                  <DataWorkingTree />
+                </p>
+                <p className="CardRight">
+                  3rd, Working Duration ( Farming or Animal Husbandry ) of Project
+                  Get Money By %
+                </p>
+              </div>
+            </Col>
+            <Col xs={{ span: 12 }} md={{ span: 6 }}>
+              <div className="CustomCardStyle">
+                <p className="CardLeft">
+                  <DataSecurityGetMoney />
+                </p>
+                <p className="CardRight">
+                  4th, After Work Done,
+                  You will Get Secure 100% Get Money
+                </p>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
+    </div>
 
-      <style jsx>
-        {`
+    <style jsx>
+      {`
 					.Container {
 						background-color: #fff;
 						background-image: url(/Index/Background.svg);
@@ -96,7 +96,7 @@ const Index = ({ t }) => {
 					}
 
 					.HowItWorksContainer {
-						padding: 0 1.5rem;
+						padding: 0 2.5rem;
 					}
 
 					.HowItWorksContainer > p {
@@ -126,6 +126,7 @@ const Index = ({ t }) => {
 						justify-content: center;
 						align-items: center;
 						width: 100%;
+						padding-left: 1px;
 						height: 123px;
 					}
 
@@ -140,10 +141,9 @@ const Index = ({ t }) => {
 						}
 					}
 				`}
-      </style>
-    </Layout>
-  )
-}
+    </style>
+  </Layout>
+)
 
 Index.getInitialProps = async () => ({
   namespacesRequired: ['common'],
