9f1a4d72502c36d67144fc75c82a9983bc25ee59
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2020-02-23 20:08:13 +0700

    Add Antd Custom Theme

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
+/* eslint-disable */
 require('dotenv').config()
+
+const withSass = require('@zeit/next-sass')
+const withCss = require('@zeit/next-css')
+const withLess = require('@zeit/next-less')
+const withPlugins = require('next-compose-plugins')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
 
 const { BASE_API_URL, PORT } = process.env
 
-module.exports = {
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './less/antd-custom.less'), 'utf8')
+)
+
+
+const nextConfig = {
   env: {
     BASE_API_URL: BASE_API_URL || 'default route',
     PORT,
@@ -25,3 +41,39 @@ module.exports = {
     return paths
   },
 }
+
+const plugins = [
+  withCss,
+  withSass,
+  withLess({
+		lessLoaderOptions: {
+			javascriptEnabled: true,
+			modifyVars: themeVariables, // make your antd custom effective
+		},
+		webpack: (config, { isServer }) => {
+			if (isServer) {
+				const antStyles = /antd\/.*?\/style.*?/
+				const origExternals = [...config.externals]
+				config.externals = [
+					(context, request, callback) => {
+						if (request.match(antStyles)) return callback()
+						if (typeof origExternals[0] === 'function') {
+							origExternals[0](context, request, callback)
+						} else {
+							callback()
+						}
+					},
+					...(typeof origExternals[0] === 'function' ? [] : origExternals),
+				]
+
+				config.module.rules.unshift({
+					test: antStyles,
+					use: 'null-loader',
+				})
+			}
+			return config
+		},
+	}),
+]
+
+module.exports = withPlugins(plugins, nextConfig)
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import App from 'next/app'
-import 'antd/dist/antd.min.css'
 import { appWithTranslation } from '../i18n'
 
 class MyApp extends App {
