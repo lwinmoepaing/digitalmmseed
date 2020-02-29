7e118fc9816d013299539693c14c7cdf25be6c6e
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2020-02-29 23:14:55 +0700

    Fixed User Projects For Farmers

--- src/pages/farmer/user-projects/index.js
+++ src/pages/farmer/user-projects/index.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import FarmerLayout from '../../layouts/FarmerLayout'
-import isPassAuth from '../../../lib/middleware/isPassAuth'
-import { withTranslation, i18n } from '../../i18n'
+import FarmerLayout from '../../../layouts/FarmerLayout'
+import isPassAuth from '../../../../lib/middleware/isPassAuth'
+import { withTranslation, i18n } from '../../../i18n'
 
 const Index = ({ t }) => (
   <FarmerLayout i18n={i18n}>
