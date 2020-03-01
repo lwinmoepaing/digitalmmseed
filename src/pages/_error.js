373c82748042fbf313fe857802f5a09a8e1e2698
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2020-03-01 14:11:20 +0700

    Latest Update

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,11 +1,24 @@
-import { BASE_API_URL } from '../../config'
+import Error404 from '../components/Common/SVG/Error404'
 
 const ErrorPage = () => (
   <div>
-    <h4>
-      Custom 404 Page
-      { BASE_API_URL }
-    </h4>
+    <div className="Container">
+      <Error404 />
+      <h3 className=""> Digital Mm Farm 404 Not Found </h3>
+      <a href="/"> Go Home </a>
+    </div>
+
+    <style jsx>
+      {`
+			.Container {
+				width: 300px;
+				margin: 0 auto;
+				margin-top: 3rem;
+				text-align: center;
+				color: green;
+			}
+		`}
+    </style>
   </div>
 )
 
